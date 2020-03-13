502767d9fe1a1ee14d396dae5edc27ad2d4e88d8
Kazuki Tanno <55737006+Kazuki-Tanno@users.noreply.github.com>
2020-03-14 02:35:33 +0900

    マップからgeojson生成を実装

--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -24,6 +24,7 @@
   </v-list>
   <br>
   <div id="map" style="width:100%; height:600px"></div>
+	<v-btn @click="MakeGeoJson">Geojson</v-btn>
 </div>
 </template>
 
@@ -40,7 +41,8 @@ export default {
       // 入力データ
       map: null,
       SpotList: null,
-			SearchString: ''
+			SearchString: '',
+			ExportItems: null
     }
   },
 
@@ -53,11 +55,32 @@ export default {
   // 監視プロパティ
   watch: {},
 
-  mounted () {
+  mounted: function() {
       // mapオブジェクトを作成
       this.map = L.map('map', { center: L.latLng( 35.6825, 139.752778 ), zoom: 15 } ).addLayer(
-        L.tileLayer( 'http://{s}.tile.osm.org/{z}/{x}/{y}.png' )
-    )
+				L.tileLayer( 'http://{s}.tile.osm.org/{z}/{x}/{y}.png' ));
+				
+			// 描画レイヤの初期化
+			var drawnItems = new L.FeatureGroup().addTo(this.map);
+			var drawControl = new L.Control.Draw({
+				draw: {
+					circle: {
+						feet: false
+					},
+					polyline: false,
+					marker: false
+				},
+				edit: {
+					featureGroup: drawnItems,
+				},
+			}).addTo(this.map);
+
+			// 描画した図形を地図上にそのまま表示
+			this.map.on('draw:created', function(e) {
+				drawnItems.addLayer(e.layer);
+			});
+
+			this.ExportItems = drawnItems
   },
 
   // メソッドプロパティ
@@ -89,7 +112,27 @@ export default {
 		// 指定した緯度経度に地図を移動
     PanMap: function(LatLon){
 			this.map.panTo(new L.LatLng(LatLon[1], LatLon[0]))
-    }
+		},
+		
+		//geojson化
+		MakeGeoJson: function(){
+			
+			var return_data = this.ExportItems.toGeoJSON()
+			if (return_data['features'].length > 0) {
+				console.log(return_data)
+				axios
+				.post('/api/post', return_data)
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(err => {
+          alert('APIサーバと接続できません')
+        })
+			}
+			else{
+				alert('geojsonを作成してください')
+			}
+		}
   }
 }
 </script>
