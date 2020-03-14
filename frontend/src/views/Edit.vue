bc9699f04d32e4f77b517486e7df90d257ca8c2f
Kazuki Tanno <55737006+Kazuki-Tanno@users.noreply.github.com>
2020-03-15 02:14:21 +0900

    Create Edit.vue

---
+++ frontend/src/views/Edit.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  this is edit.vue
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'EditPage',
+
+  // データプロパティ
+  data () {
+    return {
+      // 入力データ
+      
+    }
+  },
+
+  // ライフサイクルフック関数
+  created () {},
+
+  // 算術プロパティ
+  computed: {},
+
+  // 監視プロパティ
+  watch: {},
+
+  mounted () { },
+
+  methods: {}
+}
+</script>
