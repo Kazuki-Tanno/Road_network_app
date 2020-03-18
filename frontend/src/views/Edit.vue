65e0355334f16dff6c965a18700d78381ba2aa15
Kazuki Tanno <55737006+Kazuki-Tanno@users.noreply.github.com>
2020-03-19 03:38:51 +0900

    Update Edit.vue
    - mouseoverイベントのリスナーを追加

--- frontend/src/views/Edit.vue
+++ frontend/src/views/Edit.vue
@@ -14,12 +14,12 @@
 		background-color="primary"
 		dark
 	>
-		<v-btn value="AddNode">
+		<v-btn value="AddNode" @click="ResetPaletteSub">
 			<span class="hidden-sm-and-down">Add Node</span>
       <v-icon right>mdi-plus-circle</v-icon>
 		</v-btn>
 
-		<v-btn value="DeleteNode" >
+		<v-btn value="DeleteNode" @click="ResetPaletteSub">
 			<span class="hidden-sm-and-down">Delete Node</span>
       <v-icon right>mdi-minus-circle</v-icon>
 		</v-btn>
@@ -29,13 +29,13 @@
       <v-icon right>mdi-vector-polyline-plus</v-icon>
 		</v-btn>
 
-		<v-btn value="DeleteEdge">
+		<v-btn value="DeleteEdge" @click="ResetPaletteSub">
 			<span class="hidden-sm-and-down">Delete Edge</span>
       <v-icon right>mdi-vector-polyline-minus</v-icon>
 		</v-btn>
 	</v-btn-toggle>
-
-	{{ palette }}
+	{{ palette }} / {{ palette_sub }} /{{ now_target_node }}/
+	{{ transform.x }}/{{ transform.y }}/{{ transform.k }}
 	<br>
 
 	<svg :width="width" :height="height" :viewBox="viewBox"
@@ -43,6 +43,9 @@
 	<g class="Network" ></g>
 	</svg>
 	<br>
+	{{ test_network.node }}
+	<br>
+	{{ test_network.edge }}
 </div>
 </template>
 
@@ -62,8 +65,13 @@ export default {
 			style: "background-color: #ededed",
 			transform: {x:0, y:0, k:1},
 
-			// パレット
+			// パレットの選択状態
 			palette: null,
+			// パレットの選択のサブ状態
+			palette_sub: null,
+
+			// AddEdgeで選択されているノード
+			now_target_node: null,
 
 			// 入力データ
 			// ノードサイズ
@@ -71,13 +79,20 @@ export default {
 			// D3用のテストデータ
       test_network: {
         node: [
-					{id:0, pos:[0,0], latlon:[4,5]},
-					{id:1, pos:[200,0], latlon:[3,5]},
-					{id:2, pos:[0,200], latlon:[4,2]},
-					{id:3, pos:[200,200], latlon:[1,3]},
-					{id:4, pos:[100,50], latlon:[4,5]}
+					{id:0, pos:[0,0]},
+					{id:1, pos:[200,0]},
+					{id:2, pos:[0,200]},
+					{id:3, pos:[200,200]},
+					{id:4, pos:[100,50]}
         ],
-				edge: [[0,1],[0,2],[1,3],[2,3],[0,4],[1,4]]
+				edge: [
+					{id:0, head:0, tail:1},
+					{id:1, head:0, tail:2},
+					{id:2, head:1, tail:3},
+					{id:3, head:2, tail:3},
+					{id:4, head:0, tail:4},
+					{id:5, head:1, tail:4}
+				]
 			}
     }
   },
@@ -89,8 +104,19 @@ export default {
 		var me = this
 		document.addEventListener('keydown', (event) => {
 			if (event.key=="Escape") {
-				if (me.palette=="SearchNode"){ me.palette="AddEdge"; }
-				else{ me.palette=null; }
+				if (me.palette_sub=="SearchNode"){
+					me.palette="AddEdge";
+					me.now_target_node=null;
+					me.palette_sub=null;
+					d3.select("line.cursorLine")
+						.attr("x1", 0).attr("y1", 0).attr("x2", 0).attr("y2", 0);
+				}
+				else{
+					me.now_target_node=null;
+					me.palette_sub=null;
+					me.palette=null;
+					d3.select("circle.cursorCircle").attr("r",0);
+				}
 			} 
 		});
 	},
@@ -104,55 +130,108 @@ export default {
 	},
 
   // 監視プロパティ
-  watch: {},
+  watch: {
+		palette: function (val) {
+      if (val!=="AddEdge"){
+				this.palette_sub = null;
+				this.now_target_node = null;
+			}
+    },
+	},
 
   mounted () {
+		// vueのthisのスコープ範囲の問題を解消
+		var me = this;
+
 		//this.test_network = this.$store.state.Network
 
-		this.AddEdge();
-		this.AddNode();
+		me.InitEdge();
+		me.InitNode();
+
+		// AddNodeモードで表示するカーソル
+		d3.select("svg.Network")
+			.append("circle")
+			.attr("class","cursorCircle")
+			.attr("cx",0).attr("cy",0).attr("r",0)
+			.attr("fill","black")
+			.attr("fill-opacity", 0.7);
+
+		d3.select("svg.Network")
+			.append("line")
+			.attr("class","cursorLine")
+			.attr("x1",0).attr("y1",0).attr("x2",0).attr("y2",0)
+			.attr("stroke-dasharray", "5,5")
+			.attr("stroke", "black")
+			.attr("stroke-opacity", 0.5)
+			.attr("stroke-width", 4);
 
 		// zoomのイベント・リスナーを追加
 		d3.select("svg.Network").call(d3.zoom()
-      .extent([[0, 0], [this.width, this.height]])
+      .extent([[0, 0], [me.width, me.height]])
       .scaleExtent([0, 1000])
-			.on("zoom", this.zoomed));
+			.on("zoom", me.zoomed));
 
-		// vueのthisのスコープ範囲がおそらくd3の中身に及ばなさそう
-		var me = this;
-
-		// クリック時のイベント・リスナーを追加
+		// svg全体のクリック時イベント・リスナーを追加
 		d3.select("svg.Network").on("click", function(){
-			
-			// クリック時のposを取得
-			//var MousePos = d3.mouse(this)
-			var tl = [me.transform.x, me.transform.y]
-			var sc = me.transform.k
-			var MousePos = [(d3.mouse(this)[0] - tl[0])/sc, (d3.mouse(this)[1] - tl[1])/sc]
-			MousePos[1] = me.NodeYPos(MousePos[1])
-			// nodeデータに追加
-			var add_data = {"id":9, "pos":MousePos}
-			me.test_network.node.push(add_data)
 
-			// グループ要素を見つける
-			var g = d3.select("g.node")
-			g.selectAll("circle").data(me.test_network.node).enter().append("circle")
-			.attr("cx", ({pos}) => pos[0]).attr("cy", ({pos}) => me.YPos(pos[1]))
-			.attr("r", me.node_size)
+			if (me.palette=="AddNode") { // AddNodeモードの場合, クリックした位置にノードを追加する
+
+				var tl = [me.transform.x, me.transform.y];
+				var sc = me.transform.k;
+				var MousePos = [(d3.mouse(this)[0] - tl[0])/sc, (d3.mouse(this)[1] - tl[1])/sc];
+				// クリック位置のy座標を反転
+				MousePos[1] = me.NodeYPos(MousePos[1]);
+
+				// データにプッシュ
+				// idは現配列に格納されているidの最大値+1
+				var id = me.test_network.node.reduce((a,b)=>a.id>b.id?a:b).id + 1;
+				var add_data = {"id":id, "pos":MousePos};
+				me.test_network.node.push(add_data);
+
+				// nodeを追加
+				var g = d3.select("g.node");
+				me.AddNode(g);
+			}
+
+		})
+		.on("mousemove", function(){
+			var tl = [me.transform.x, me.transform.y];
+			var sc = me.transform.k;
+
+			if (me.palette_sub==="SearchNode") {
+				var pre_select = me.GetNodePos(me.test_network.node, me.now_target_node)
+				d3.select("line.cursorLine")
+					.attr("x1", (pre_select[0]*sc)+tl[0])
+					.attr("y1", (me.YPos(pre_select[1])*sc+tl[1]))
+					.attr("x2", d3.mouse(this)[0])
+					.attr("y2", d3.mouse(this)[1]);
+			} else if (me.palette==="AddNode") {
+
+				d3.select("circle.cursorCircle")
+					.attr("cx",d3.mouse(this)[0]).attr("cy",d3.mouse(this)[1]).attr("r",5);
+			} else {
+				true
+			}
+		})
+		.on("mouseout", function(){
+			d3.select("line.cursorLine")
+				.attr("x1", 0).attr("y1", 0).attr("x2", 0).attr("y2", 0);
+			d3.select("circle.cursorCircle").attr("r",0);
 		});
 	},
 	
   methods: {
 		// zoom関数
 		zoomed: function() {
-			this.transform = d3.event.transform;
+			var me = this
+			me.transform = d3.event.transform;
 			d3.select("g.Network").attr("transform", d3.event.transform);
 		},
 
 		ChangeSVG: function(n){
 				this.height += n
 				this.width += n
-			},
+		},
 
 		BiggerNode: function(n){
 			this.node_size += n
@@ -175,77 +254,169 @@ export default {
 		},
 
 		// yのposを反転して返すメソッド
-		YPos: function(v){
-			return this.height - v
-		},
+		YPos: function(v){ return this.height - v },
 
 		// 画面の位置からYposを返す関数
-		NodeYPos: function(v){
-			return this.height - v
+		NodeYPos: function(v){ return this.height - v; },
+
+		// 常に追加してあるエッジか否かを判定する関数
+		CanAddEdge: function(v, h){
+
+			var result = true
+
+			if(v===h){
+				result = false;
+			}else{
+				var me = this;
+				var edge_list = [];
+				me.test_network.edge.forEach(element => {
+					if (element.head===v ) { edge_list.push(element.tail)}
+					else if ( element.tail===v ) { edge_list.push(element.head)}
+				});
+				// 重複削除
+				edge_list = edge_list.filter(function (x, i, self) {
+					return self.indexOf(x) === i;
+				});
+
+				// 追加可能なエッジか判別
+				result = true;
+				edge_list.forEach(element => {
+					if (element===h) { result = false;}
+				});
+			}
+
+			return result
 		},
 
-		AddEdge: function(){
-			
-			var g = d3.select("g.Network")
+		ResetPaletteSub: function(){
+			this.palette_sub = null;
+			this.now_target_node = null;
+		},
 
-			// svgのグループを作成
-			const edge = g.append("g").attr("class", "edge")
+		// ノードを追加するメソッド(クリック時の挙動も定義するので関数化する)
+		AddNode: function(g){
+			var me = this;
+
+			g.selectAll("circle").data(me.test_network.node, function(d){return d.id;}).enter().append("circle")
+				.attr("cx", ({pos}) => pos[0]).attr("cy", ({pos}) => me.YPos(pos[1]))
+				.attr("r", me.node_size)
+				.on("click",function(d,idx,elem){
+
+					if (me.palette==null){ // 編集モードが何も選択されていないとき
+						var msg = "data:" + d.pos + " idx:" + idx + " elem:" + elem + " d3.event.pageX: " + d3.event.pageX + " d3.mouse: " + d3.mouse(this);
+						alert(msg);
+					} else if (me.palette=="AddEdge"){ // edgeを追加しているとき
+						if (me.palette_sub=="SearchNode") { // tailノードを探しているとき
+							if (me.CanAddEdge(me.now_target_node, d.id)) { // もしedgeをちゃんと追加できるなら
+
+								var id = me.test_network.edge.reduce((a,b)=>a.id>b.id?a:b).id + 1
+								var add_data = {"id":id, "head":me.now_target_node, "tail":d.id}
+								me.test_network.edge.push(add_data)
+
+								// エッジを追加
+								me.AddEdge(d3.select("g.edge"))
+
+								d3.select("line.cursorLine")
+									.attr("x1", 0).attr("y1", 0).attr("x2", 0).attr("y2", 0);
+
+								me.palette_sub = null;
+								me.now_target_node = null;
+							}
+						} else {
+							me.now_target_node = d.id
+							me.palette_sub = "SearchNode";
+						}
+						
+					} else if (me.palette=="DeleteNode"){ // ノード削除モードのとき
+
+						// ノードを削除
+						me.DeleteNode(g, d.id);
+
+						// 隣接したエッジを削除
+						// 削除するエッジのidのリストを作成
+						var delete_list = []
+						me.test_network.edge.forEach(element => {
+							if (element.head===d.id || element.tail===d.id ) {
+								delete_list.push(element.id)
+							}
+						});
+						delete_list = delete_list.filter(function (x, i, self) {
+							return self.indexOf(x) === i;
+						});
+						var edge_g = d3.select("g.edge")
+						delete_list.forEach(element => {
+							me.DeleteEdge(edge_g, element)
+						});
+
+					}
+				});
+		},
+
+		AddEdge: function(g){
+			var me = this
+
+			g.selectAll("line")
+				.data(me.test_network.edge, function(d){return d.id}).enter()
+				.append("line")
+				.attr("x1", function(d){return me.GetNodePos(me.test_network.node, d.head)[0]})
+				.attr("y1", function(d){return me.YPos(me.GetNodePos(me.test_network.node, d.head)[1])})
+				.attr("x2", function(d){return me.GetNodePos(me.test_network.node, d.tail)[0]})
+				.attr("y2", function(d){return me.YPos(me.GetNodePos(me.test_network.node, d.tail)[1])})
+				.attr("stroke-width", 2)
+				.attr("stroke", "black")
+				.on("click",function(d,idx,elem){
+					if (me.palette===null) {
+						var msg = "data:" + d + " idx:" + idx + " elem:" + elem + " d3.event.pageX: " + d3.event.pageX + " d3.mouse: " + d3.mouse(this) ;
+						alert(msg);
+					} else if (me.palette==="DeleteEdge"){
+						me.DeleteEdge(g, d.id)
+					}
+				});
+
+		},
+
+		// ノードを削除するメソッド
+		DeleteNode: function(g, node_id){// node_idは削除するノードのid
+			var me = this;
+			// nodeリストから削除
+			me.test_network.node = me.test_network.node.filter( function(item){ return item.id!==node_id; } );
+			// 消されたリストを反映
+			// data部分はidで設定しないと上手く消せない
+			g.selectAll("circle").data(me.test_network.node, function(d){return d.id;}).exit().remove();
+		},
+
+		// エッジを削除するメソッド
+		DeleteEdge: function(g, edge_id){
+			var me = this;
+			// edgeリストから削除
+			me.test_network.edge = me.test_network.edge.filter( function(item){ return item.id!==edge_id; } );
+			// 消されたリストを反映
+			g.selectAll("line").data(me.test_network.edge, function(d){ return d.id; }).exit().remove();
+		},
+
+		InitEdge: function(){
 
 			// d3のfunction内でthisに指定する関数は使えない模様
 			var me = this
 
+			// svgのグループを作成
+			var g = d3.select("g.Network")
+			const edge = g.append("g").attr("class", "edge")
+
 			// edgeを描画
-			edge.selectAll("line")
-				.data(me.test_network.edge).enter()
-				.append("line")
-				.attr("style", "stroke:#000;stroke-width:5")
-				.attr("x1", function(d){return me.GetNodePos(me.test_network.node, d[0])[0]})
-				.attr("y1", function(d){return me.YPos(me.GetNodePos(me.test_network.node, d[0])[1])})
-				.attr("x2", function(d){return me.GetNodePos(me.test_network.node, d[1])[0]})
-				.attr("y2", function(d){return me.YPos(me.GetNodePos(me.test_network.node, d[1])[1])})
-				.on("click",function(d,idx,elem){
-					var msg = "data:" + d + " idx:" + idx + " elem:" + elem + " d3.event.pageX: " + d3.event.pageX + " d3.mouse: " + d3.mouse(this) ;
-					alert(msg);
-				});
+			me.AddEdge(edge)
 		},
 
-		AddNode:function(){
+		InitNode:function(){
+
+			var me = this;
 			
 			// svgを検索
-			var svg = d3.select("g.Network")
-			var YPos = this.YPos
+			var svg = d3.select("g.Network");
 
 			// svgのグループを作成
-			const node = svg.append("g").attr("class", "node")
-
-			// nodeを描画
-			node.selectAll("circle")
-        .data(this.test_network.node)
-        .enter()
-        .append("circle").attr("cx", ({pos}) => pos[0]).attr("cy", ({pos}) => YPos(pos[1]))
-        .attr("r", this.node_size).attr("fill", "#000000")
-				// クリックイベントの追加
-				.on("click",function(d,idx,elem){
-					var msg = "data:" + d.pos + " idx:" + idx + " elem:" + elem + " d3.event.pageX: " + d3.event.pageX + " d3.mouse: " + d3.mouse(this) ;
-					alert(msg);
-				});
-
-			/*
-			// テキストを表示
-			const text = svg.append("g")
-
-			// テキストを描画
-			text.selectAll("text")
-				.data(this.test_network.node)
-				.enter()
-				.append("text").text( 
-					function(d){ return d.id + ": [" + String(d.pos) + "]" }
-				)
-				.attr("x", ({pos}) => pos[0]).attr("y", ({pos}) => YPos(pos[1]))
-				.attr("font-family", "sans-serif").attr("font-size", "10px")
-				.attr("text-anchor", "middle").attr("dominant-baseline", "central")
-				.attr("fill", "white");
-				*/
+			const g = svg.append("g").attr("class", "node");
+			me.AddNode(g);
 		}
 	}
 }
