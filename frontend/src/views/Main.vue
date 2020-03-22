68bcfaef5dc944c46679891b4755b6be36aa1ca9
Kazuki Tanno <55737006+Kazuki-Tanno@users.noreply.github.com>
2020-03-22 15:23:02 +0900

    Create Main.vue

---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,33 @@
+<template>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HomePage',
+
+  // データプロパティ
+  data () {
+    return {
+      // 入力データ
+      
+    }
+  },
+
+  // ライフサイクルフック関数
+  created () {},
+
+  // 算術プロパティ
+  computed: {},
+
+  // 監視プロパティ
+  watch: {},
+
+  mounted () { },
+
+  methods: {
+    
+  }
+}
+</script>
